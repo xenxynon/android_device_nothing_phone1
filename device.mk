711960d7d1e01201deb29d9a63653098ab0746c4
SamarV-121 <samar@samarv121.dev>
2024-09-23 02:29:08 +0900

    phone1: Link older libcrypto for widevine blobs
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    
    Change-Id: Ic32d5103d42cd28ccbc8055e766288dfefc1c02d
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -87,7 +87,8 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    libcrypto-v33
 
 # EGL - Blobcache configuration
 PRODUCT_VENDOR_PROPERTIES += \
