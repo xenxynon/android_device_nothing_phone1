9fcf3573c1bb5337cacb70be3d39d8a80915d7f7
xenxynon <flynryder427@gmail.com>
2024-11-15 12:09:39 +0530

    phone1: build vendor.qti.hardware.perf@2.2.vendor
    Change-Id: I0a5d540c699a00b09076b398b32eb10a71bb52e5

--- device.mk
+++ device.mk
@@ -221,6 +221,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += $(DEVICE_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Powershare
 PRODUCT_PACKAGES += \
     vendor.aospa.powershare-service
