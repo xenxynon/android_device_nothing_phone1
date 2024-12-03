188718eb9a5eff607a74cf4bf3809adcbaf1b70c
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-12-03 19:09:12 +0530

    art: Disable ART ndebug as well We already disabled debugging art for both host as well as target so it makes no sense to have runtime debugging features enabled as well.
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: Idc09b056ac2ba5b937e1eecd40a0d44982a37900
    Reviewed-on: https://review.statixos.com/c/android_art/+/5370
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Reviewed-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- device.mk
+++ device.mk
@@ -64,9 +64,9 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Dex-preopt/Art
-ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_NDEBUG := false
 ART_BUILD_TARGET_DEBUG := false
-ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_NDEBUG := false
 ART_BUILD_HOST_DEBUG := false
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := everything
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
